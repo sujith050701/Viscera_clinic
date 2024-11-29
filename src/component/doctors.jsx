80207e8c0ca7fd7147daf341472336f15a4ffc06
sujith050701 <rsujith776@gmail.com>
2024-11-29 21:56:26 +0530

    second commit

--- src/component/doctors.jsx
+++ src/component/doctors.jsx
@@ -14,8 +14,8 @@ function Doctor(){
       <div className="row g-5">
         <div className="col-lg-4 wow slideInUp" data-wow-delay="0.1s">
           <div className="section-title bg-light rounded h-100 p-5">
-            <h5 className="position-relative d-inline-block text-primary text-uppercase">Our Dentist</h5>
-            <h1 className="display-6 mb-4">Meet Our Certified &amp; Experienced Dentist</h1>
+            <h5 className="position-relative d-inline-block text-primary text-uppercase">Our Doctors</h5>
+            <h1 className="display-6 mb-4">Meet Our Certified &amp; Experienced Doctors </h1>
             <Link to="/appointment" className="btn btn-primary py-3 px-5">Appointment</Link>
           </div>
         </div>
